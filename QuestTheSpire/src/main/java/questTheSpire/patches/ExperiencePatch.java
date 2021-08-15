3a172d355c6ffb813d601ebb45742e308e74b1d2
Alison Turner <alisonmoons@gmail.com>
2021-08-15 15:37:04 -0300

    Added comments

--- QuestTheSpire/src/main/java/questTheSpire/patches/ExperiencePatch.java
+++ QuestTheSpire/src/main/java/questTheSpire/patches/ExperiencePatch.java
@@ -20,16 +20,22 @@ import static questTheSpire.QuestTheSpire.*;
 public class ExperiencePatch {
     @SpirePostfixPatch
     public static void calculateUnlockProgress(GameOverScreen __instance) {
+        //Grab the current level and prestige, we could inline this but its easier to read this way
         int currentLevel = activeCharacterFile.getLevel();
         int currentPrestige = activeCharacterFile.getPrestigeLevel();
+        //Determine how much exp we just made
         int xp = GameOverScreen.calcScore(isVictory);
+        //See what our level would be with the new exp to see how many times we have levelled up. Then do the same for prestige
         int levelUps = currentLevel - CharacterSaveFile.calculateLevel(activeCharacterFile.getExp()+xp);
         int prestigeUps = currentPrestige - CharacterSaveFile.calculatePrestigeLevel(activeCharacterFile.getExp()+xp);
+        //Add the xp to our character
         activeCharacterFile.addExp(xp);
+        //If we actually levelled up, add the number of levels we went up and add out perk points and all that
         if (levelUps > 0) {
             //TODO add perk points and whatever unlocks at certain level
             activeCharacterFile.addLevel(levelUps);
         }
+        //If our prestige went up, same dice
         if (prestigeUps > 0) {
             //TODO add perk points from prestige up
             activeCharacterFile.addPrestigeLevel(prestigeUps);
