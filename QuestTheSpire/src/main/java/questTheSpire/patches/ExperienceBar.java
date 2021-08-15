228a0fa0a85822019f5f3e50f3bbde364f5ae67d
Azoth658 <Azoth658@gmail.com>
2021-08-15 20:18:20 +0100

    Added Denum and Num to XP Bar

--- QuestTheSpire/src/main/java/questTheSpire/patches/ExperienceBar.java
+++ QuestTheSpire/src/main/java/questTheSpire/patches/ExperienceBar.java
@@ -13,6 +13,7 @@ import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.screens.GameOverScreen;
 import questTheSpire.Level.LevelCosts;
+import questTheSpire.util.CharacterSaveFile;
 
 import static basemod.DevConsole.unlockLevel;
 import static questTheSpire.QuestTheSpire.*;
@@ -27,34 +28,28 @@ public class ExperienceBar {
         sb.draw(ImageMaster.WHITE_SQUARE_IMG, ___progressBarX, (float) Settings.HEIGHT * 0.3F, ___progressBarWidth, 14.0F * Settings.scale);
 
         //TODO get level, numerator, and denominator from save file
-        try {
-            AbstractPlayer.PlayerClass pc = AbstractDungeon.player.chosenClass;
-            SpireConfig config = new SpireConfig("QuestTheSpire", pc.toString() + "_QuestTheSpire_Stats", questTheSpireCharacterStats);
-            Level = config.getInt("Level");
-            OverFlowExperience = config.getInt("OverFlowExperience");
-            config.load();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //Level Green = R-0.0, G-0.8, B-0.3
-        //Prestige Blue = R-0.0, G-1.57 B-1.96
-        if (Level==20) {
-            sb.setColor(new Color(0.0F, 1.57F, 1.96F, ___progressBarAlpha * 0.9F)); //TODO 157 green and 196 blue do not map to 1.57F and 1.96F, they map to 0.62F and 0.77F (the value is: color / 255)
+        int currentLevel = activeCharacterFile.getLevel();
+        int currentPrestige = activeCharacterFile.getPrestigeLevel();
+        int current = CharacterSaveFile.getExpBarNumerator(activeCharacterFile.getExp());
+        int next = CharacterSaveFile.getExpBarDenominator(activeCharacterFile.getExp());
+
+        if (currentLevel==20) {
+            sb.setColor(new Color(0.0F, 0.62F, 0.77F, ___progressBarAlpha * 0.9F)); //157 green and 196 blue do not map to 1.57F and 1.96F, they map to 0.62F and 0.77F (the value is: color / 255)
         }   else sb.setColor(new Color(0.0F, 0.8F, 0.3F, ___progressBarAlpha * 0.9F));
 
         sb.draw(ImageMaster.WHITE_SQUARE_IMG, ___progressBarX, (float)Settings.HEIGHT * 0.3F, ___progressBarWidth * ___progressPercent, 14.0F * Settings.scale);
         sb.setColor(new Color(0.0F, 0.0F, 0.0F, ___progressBarAlpha * 0.25F));
         sb.draw(ImageMaster.WHITE_SQUARE_IMG, ___progressBarX, (float)Settings.HEIGHT * 0.3F, ___progressBarWidth * ___progressPercent, 5.0F * Settings.scale);
 
-        String derp = "[" + (int)(Experience) + "/" + LevelCosts.levelcost(LevelRequirement) + "]";
+        String derp = "[" + (int)(current) + "/" + (int)(next)+ "]";
         ___creamUiColor.a = ___progressBarAlpha * 0.9F;
         FontHelper.renderFontLeftTopAligned(sb, FontHelper.topPanelInfoFont, derp, 576.0F * Settings.xScale, (float)Settings.HEIGHT * 0.3F - 12.0F * Settings.scale, ___creamUiColor);
 
         //TODO localization
         if (Level < 20) {
-            derp = "Level " + (Level+1);
+            derp = "Level " + (currentLevel+1);
         } else {
-            derp = "Prestige " + (Level+1);
+            derp = "Prestige " + (currentPrestige+1);
         }
 
         FontHelper.renderFontRightTopAligned(sb, FontHelper.topPanelInfoFont, derp, 1344.0F * Settings.xScale, (float)Settings.HEIGHT * 0.3F - 12.0F * Settings.scale, ___creamUiColor);
