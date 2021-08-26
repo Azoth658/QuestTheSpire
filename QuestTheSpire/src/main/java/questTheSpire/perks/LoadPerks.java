0f52c7e97579412d324b643b3973fcb9ccac942b
Azoth658 <Azoth658@gmail.com>
2021-08-26 22:38:43 +0100

    Initial LoadPerks

--- QuestTheSpire/src/main/java/questTheSpire/perks/LoadPerks.java
+++ QuestTheSpire/src/main/java/questTheSpire/perks/LoadPerks.java
@@ -1,4 +1,18 @@
 package questTheSpire.perks;
 
-public class LoadPerks {
+import basemod.interfaces.PostInitializeSubscriber;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class LoadPerks implements PostInitializeSubscriber {
+    public boolean perkloaded;
+    public int hpPerk;
+    public int goldPerk;
+    public int strPerk;
+    public int dexPerk;
+    public int focusPerk;
+
+    @Override
+    public void receivePostInitialize() {
+        AbstractDungeon.player.gainGold(2000);
+    }
 }
