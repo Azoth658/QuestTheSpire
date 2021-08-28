7a252f69af07e58a1c59a6c46358fdfa81f69dde
Alison Turner <alisonmoons@gmail.com>
2021-08-28 12:08:24 -0300

    Added additional hooks and hover logic

--- QuestTheSpire/src/main/java/questTheSpire/screens/ClickableLoadoutOption.java
+++ QuestTheSpire/src/main/java/questTheSpire/screens/ClickableLoadoutOption.java
@@ -11,50 +11,64 @@ import com.megacrit.cardcrawl.helpers.Hitbox;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 
+import static questTheSpire.screens.CharacterLoadout.PERK_IMAGE;
+
 public abstract class ClickableLoadoutOption {
-    private String name;
     private String label;
     private float x;
     private float y;
     private Button lb;
     private Button rb;
-    private static float LABEL_Y_OFFSET = -3f;
     private boolean updateNeeded;
+    private boolean canUp, canDown;
 
-    public ClickableLoadoutOption (String name, float x, float y) {
-        this.name = name;
+    public ClickableLoadoutOption (float x, float y) {
         this.x = x;
         this.y = y;
-        label = makeLabel(name);
+        label = makeLabel();
         lb = new Button(false, 0, 0);
         rb = new Button(true, 0, 0);
-        lb.move(x, y);
-        rb.move(x+FontHelper.getWidth(FontHelper.cardTitleFont, label, Settings.scale)+2*lb.w, y);
+        lb.move(x-getAssembledWidth()/2f, y);
+        rb.move(x+getAssembledWidth()/2f, y);
+        canUp = canUpgrade();
+        canDown = canDowngrade();
     }
 
     public abstract void onClickArrow(boolean increase);
 
-    public abstract String makeLabel(String name);
+    public abstract String makeLabel();
+
+    public abstract int getUpgradeCost();
+
+    public abstract int getDowngradeRefund();
+
+    public abstract boolean canUpgrade();
+
+    public abstract boolean canDowngrade();
+
+    public abstract int amountPerLevel();
 
     public void update() {
         lb.update();
         rb.update();
         if (updateNeeded) {
             updateNeeded = false;
-            label = makeLabel(name);
-            lb.move(x, y);
-            rb.move(x+FontHelper.getWidth(FontHelper.cardTitleFont, label, Settings.scale)+2*lb.w, y);
+            label = makeLabel();
+            canUp = canUpgrade();
+            canDown = canDowngrade();
+            lb.move(x-getAssembledWidth()/2f, y);
+            rb.move(x+getAssembledWidth()/2f, y);
         }
     }
 
     public void render(SpriteBatch sb) {
         lb.render(sb);
-        FontHelper.renderSmartText(sb, FontHelper.cardTitleFont, label, x + lb.w/2f, y + LABEL_Y_OFFSET, 99999.0F, 30.0F * Settings.scale, Settings.GOLD_COLOR);
+        FontHelper.renderFontCentered(sb, FontHelper.cardTitleFont, label, x, y, Settings.GOLD_COLOR, Settings.scale);
         rb.render(sb);
     }
 
     public float getAssembledWidth() {
-        return 3*lb.w/2f + 3*rb.w/2f + FontHelper.getWidth(FontHelper.cardTitleFont, label, Settings.scale);
+        return lb.w + rb.w + FontHelper.getWidth(FontHelper.cardTitleFont, label, Settings.scale);
     }
 
     private class Button implements IUIElement {
@@ -65,6 +79,7 @@ public abstract class ClickableLoadoutOption {
         private final int h;
         private final Hitbox hitbox;
         private final boolean rightButton;
+        private boolean enabled;
 
         public Button(boolean rightButton, float x, float y) {
             this.rightButton = rightButton;
@@ -79,25 +94,34 @@ public abstract class ClickableLoadoutOption {
         public void move(float newX, float newY) {
             this.x = (int)(newX - (float)this.w / 2.0F);
             this.y = (int)(newY - (float)this.h / 2.0F);
-            this.hitbox.move(newX - (float)this.w / 2.0F, newY - (float)this.h / 2.0F);
+            this.hitbox.move(newX, newY);
         }
 
         public void render(SpriteBatch sb) {
-            if (this.hitbox.hovered) {
-                sb.setColor(Color.WHITE);
-            } else {
-                sb.setColor(Color.LIGHT_GRAY);
+            if (enabled) {
+                float halfW = (float)this.arrow.getWidth() / 2.0F;
+                float halfH = (float)this.arrow.getHeight() / 2.0F;
+                if (this.hitbox.hovered) {
+                    sb.setColor(Color.WHITE);
+                    if (rightButton) {
+                        sb.draw(PERK_IMAGE, x + 2*this.w - PERK_IMAGE.getRegionWidth()/2F, y + halfH/2f - PERK_IMAGE.getRegionHeight()/2F, PERK_IMAGE.getRegionWidth()/2F, PERK_IMAGE.getRegionHeight()/2F, PERK_IMAGE.getRegionWidth(), PERK_IMAGE.getRegionHeight(), Settings.scale/2f, Settings.scale/2f, 0.0F);
+                        FontHelper.renderFontCentered(sb, FontHelper.cardTitleFont, String.valueOf(getUpgradeCost()), x+3*this.w, y, Settings.GOLD_COLOR, Settings.scale);
+                    } else {
+                        sb.draw(PERK_IMAGE, x - this.w - PERK_IMAGE.getRegionWidth()/2F, y + halfH/2f - PERK_IMAGE.getRegionHeight()/2F, PERK_IMAGE.getRegionWidth()/2F, PERK_IMAGE.getRegionHeight()/2F, PERK_IMAGE.getRegionWidth(), PERK_IMAGE.getRegionHeight(), Settings.scale/2f, Settings.scale/2f, 0.0F);
+                        FontHelper.renderFontCentered(sb, FontHelper.cardTitleFont, String.valueOf(getDowngradeRefund()), x-2*this.w, y, Settings.GOLD_COLOR, Settings.scale);
+                    }
+                } else {
+                    sb.setColor(Color.LIGHT_GRAY);
+                }
+                sb.draw(this.arrow, this.x - halfW/2f, this.y - halfH/2f, halfW, halfH, (float)this.arrow.getWidth(), (float)this.arrow.getHeight(), Settings.scale, Settings.scale, 0.0F, 0, 0, this.arrow.getWidth(), this.arrow.getHeight(), true, false);
+                this.hitbox.render(sb);
             }
-
-            float halfW = (float)this.arrow.getWidth() / 2.0F;
-            float halfH = (float)this.arrow.getHeight() / 2.0F;
-            sb.draw(this.arrow, this.x - halfW, this.y - halfH, halfW, halfH, (float)this.arrow.getWidth(), (float)this.arrow.getHeight(), Settings.scale, Settings.scale, 0.0F, 0, 0, this.arrow.getWidth(), this.arrow.getHeight(), true, false);
-            this.hitbox.render(sb);
         }
 
         public void update() {
+            enabled = rightButton ? canUp : canDown;
             this.hitbox.update();
-            if (this.hitbox.hovered && InputHelper.justClickedLeft) {
+            if (this.hitbox.hovered && InputHelper.justClickedLeft && enabled) {
                 CardCrawlGame.sound.play("UI_CLICK_1");
                 onClickArrow(rightButton);
                 updateNeeded = true;
