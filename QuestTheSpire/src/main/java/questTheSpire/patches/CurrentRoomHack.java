8fd16899aa35f7575b269b420a6822f5cac6a009
Alison Turner <alisonmoons@gmail.com>
2021-08-29 14:08:06 -0300

    Added bypass for current room

---
+++ QuestTheSpire/src/main/java/questTheSpire/patches/CurrentRoomHack.java
@@ -0,0 +1,25 @@
+package questTheSpire.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.rooms.EmptyRoom;
+
+public class CurrentRoomHack {
+    public static boolean roomHack = false;
+    private static final AbstractRoom EMPTY_ROOM = new EmptyRoom();
+
+    //Stop adding relics from crashing
+    @SpirePatch(clz = AbstractDungeon.class, method = "getCurrRoom")
+    public static class GimmeRoom {
+        @SpirePrefixPatch
+        public static SpireReturn<?> withoutCrashingPls(AbstractDungeon __instance) {
+            if (roomHack) {
+                return SpireReturn.Return(EMPTY_ROOM);
+            }
+            return SpireReturn.Continue();
+        }
+    }
+}
