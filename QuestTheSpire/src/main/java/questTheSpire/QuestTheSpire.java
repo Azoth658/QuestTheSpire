f0c29ebe507a03b8ddaa215ba0db3e0961d2a31f
Azoth658 <Azoth658@gmail.com>
2021-09-05 15:35:19 +0100

    Pulled current events until reworked

--- QuestTheSpire/src/main/java/questTheSpire/QuestTheSpire.java
+++ QuestTheSpire/src/main/java/questTheSpire/QuestTheSpire.java
@@ -353,6 +353,7 @@ public class QuestTheSpire implements
         
         // =============== EVENTS =================
 
+        /* PULLING CURRENT EVENTS UNTIL REWORKED
         BaseMod.addEvent(new AddEventParams.Builder(wanderingMerchant.ID, wanderingMerchant.class).eventType(EventUtils.EventType.NORMAL).bonusCondition(() -> AbstractDungeon.player.gold >= 50).dungeonID(Exordium.ID).create());
         BaseMod.addEvent(druidEvent.ID, druidEvent.class, Exordium.ID);
         BaseMod.addEvent(fairyEvent.ID, fairyEvent.class, TheCity.ID);
@@ -360,6 +361,7 @@ public class QuestTheSpire implements
         BaseMod.addEvent(new AddEventParams.Builder(blacksmithEvent.ID, blacksmithEvent.class).dungeonID(TheBeyond.ID).eventType(EventUtils.EventType.NORMAL).bonusCondition(() -> activeCharacterFile.getLevel() == 20).create());
         BaseMod.addEvent(new AddEventParams.Builder(dragonHoard.ID, dragonHoard.class).eventType(EventUtils.EventType.NORMAL).bonusCondition(() -> AbstractDungeon.player.gold > 1000).create());
         BaseMod.addEvent(new AddEventParams.Builder(alchemyLab.ID, alchemyLab.class).eventType(EventUtils.EventType.NORMAL).bonusCondition(() -> AbstractDungeon.player.potionSlots > 0 && !AbstractDungeon.player.hasRelic(Sozu.ID)).dungeonID(TheCity.ID).create());
+        */
 
         // =============== SAVABLES =================
         logger.info("Preparing CustomSavables");
